b6a61de4624e3549d7eaee1642682fb7bdc4cbdb
Jonson B <www.jbc.bd@gmail.com>
2025-02-14 07:13:19 +0600

    refactor: Modified perfectionist's configs

--- src/cli/utils.ts
+++ src/cli/utils.ts
@@ -1,26 +1,5 @@
 import { execSync } from 'node:child_process';
 
-export function isGitClean(): boolean {
-  try {
-    execSync('git diff-index --quiet HEAD --');
-    return true;
-  }
-  catch {
-    return false;
-  }
-}
-
-export function getJetbrainsEslintConfigContent(configFrom: string): string {
-  return `
-import { whoj } from '@whoj/eslint-config';
-import _defaults from "${configFrom}";
-
-export default whoj({
-  isInEditor: true
-}).append(_defaults);
-`.trimStart();
-}
-
 export function getEslintConfigContent(
   mainConfig: string,
   additionalConfigs?: string[]
@@ -32,4 +11,41 @@ export default whoj({
 ${mainConfig}
 }${additionalConfigs?.map(config => `,{\n${config}\n}`)});
 `.trimStart();
+}
+
+export function getJetbrainsEslintConfigContent(configFrom: string): string {
+  return `
+import whoj from "${configFrom}";
+
+export default whoj.clone().overrides({
+  'whoj/javascript/rules': {
+    rules: {
+      'prefer-const': 'warn',
+        'unused-imports/no-unused-imports': 'warn'
+    }
+  },
+  'whoj/test/rules': {
+    rules: {
+      'test/no-only-tests': 'warn',
+      'test/consistent-test-it': ['error', { fn: 'it', withinDescribe: 'it' }],
+    }
+  }
+}).disableRulesFix([
+  'unused-imports/no-unused-imports',
+  'test/no-only-tests',
+  'prefer-const'
+], {
+  builtinRules: () => import(['eslint', 'use-at-your-own-risk'].join('/')).then(r => r.builtinRules)
+});
+`.trimStart();
+}
+
+export function isGitClean(): boolean {
+  try {
+    execSync('git diff-index --quiet HEAD --');
+    return true;
+  }
+  catch {
+    return false;
+  }
 }
--- src/configs/perfectionist.ts
+++ src/configs/perfectionist.ts
@@ -1,6 +1,10 @@
+import type { Linter } from 'eslint';
+
 import type { TypedFlatConfigItem } from '../types';
 
 import { pluginPerfectionist } from '../plugins';
+
+const LINE_LENGTH_REGEX = /.*(?<!classes|heritage-clauses|intersection-types|interfaces|modules|objects)$/;
 
 /**
  * Perfectionist plugin for props and items sorting.
@@ -8,39 +12,37 @@ import { pluginPerfectionist } from '../plugins';
  * @see https://github.com/azat-io/eslint-plugin-perfectionist
  */
 export function perfectionist(): TypedFlatConfigItem[] {
-  const { name, rules, ...config } = pluginPerfectionist.configs['recommended-line-length'];
-
   return [
     {
-      ...config,
       name: 'whoj/perfectionist/setup',
-      rules: { // @ts-ignore
-        ...Object.entries(rules!).reduce((_rules, [rule, [severity, options]]) => {
-          _rules[rule] = [severity, { ...options, order: 'asc' }];
-          return _rules;
-        }, {} as Exclude<typeof rules, undefined>)
-        // 'perfectionist/sort-exports': ['error', { order: 'asc', type: 'line-length' }],
-        // 'perfectionist/sort-objects': ['error', { order: 'asc', type: 'line-length' }],
-        // 'perfectionist/sort-interfaces': ['error', { order: 'asc', type: 'line-length' }],
-        // 'perfectionist/sort-object-types': ['error', { order: 'asc', type: 'line-length' }],
-        // 'perfectionist/sort-named-exports': ['error', { order: 'asc', type: 'line-length' }],
-        // 'perfectionist/sort-named-imports': ['error', { order: 'asc', type: 'line-length' }],
-        // 'perfectionist/sort-intersection-types': ['error', { order: 'asc', type: 'alphabetical' }],
-        // 'perfectionist/sort-imports': ['error', {
-        //   order: 'asc',
-        //   type: 'line-length',
-        //   newlinesBetween: 'ignore',
-        //   groups: [
-        //     ['type', 'parent-type', 'sibling-type', 'index-type', 'internal-type'],
-        //
-        //     ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
-        //
-        //     'side-effect',
-        //     'object',
-        //     'unknown'
-        //   ]
-        // }]
+      plugins: {
+        perfectionist: pluginPerfectionist
+      },
+      rules: {
+        ...getRules({ order: 'asc', type: 'natural' }),
+        ...getRules({ order: 'asc', type: 'line-length' })
       }
     }
   ];
+}
+
+function getRules(
+  options: {
+    order: 'asc' | 'desc';
+    type: 'natural' | 'line-length';
+  }
+): Linter.RulesRecord {
+  return Object.fromEntries(
+    Object.keys({ ...pluginPerfectionist.rules! }).filter(
+      ruleName => options.type === 'natural'
+        ? !LINE_LENGTH_REGEX.test(ruleName) && !ruleName.endsWith('sort-modules')
+        : LINE_LENGTH_REGEX.test(ruleName)
+    )
+      .map(
+        ruleName => [
+          `${pluginPerfectionist.name}/${ruleName}`,
+          ['error', options]
+        ]
+      )
+  );
 }
